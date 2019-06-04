3e23485199e545fc93cbbf76b36386f8d7a2d7b3
admin <wo.xiaoyao.zizai@163.com>
2019-06-04 15:39:53 +0800

    加了点注释

--- src/main/java/filter/DifferentCourseSwapMoveFilter.java
+++ src/main/java/filter/DifferentCourseSwapMoveFilter.java
@@ -10,6 +10,8 @@ import org.optaplanner.core.impl.score.director.ScoreDirector;
  * Created by xcy on 2019/5/21.
  */
 public class DifferentCourseSwapMoveFilter implements SelectionFilter<TimeTablingProblem, SwapMove> {
+
+    //交换同一个课程的不同课时，对分值没有影响。所以需要过滤掉它。
     @Override
     public boolean accept(ScoreDirector<TimeTablingProblem> scoreDirector, SwapMove move) {
         Lecture leftLecture = (Lecture) move.getLeftEntity();
