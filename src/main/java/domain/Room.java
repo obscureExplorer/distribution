3dc63b87494a8ddafadf0e6b33c621efc3db5c0f
admin <wo.xiaoyao.zizai@163.com>
2019-06-04 15:35:26 +0800

    Room类重写了equals和hashcode

--- src/main/java/domain/Room.java
+++ src/main/java/domain/Room.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by xcy on 2019/5/20.
@@ -31,5 +32,18 @@ public class Room implements Serializable {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(name, room.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
